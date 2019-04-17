9372d84e04a44f7c431ad29a0ed15117332ebf6e
Ahmedhaseeb <ahmedhaseeb123@gmail.com>
2019-04-17 10:46:52 +0500

    Initial commit

---
+++ php/lecture1.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>PHP - Lecture 1</title>
+</head>
+<body>
+
+	<?php
+
+
+		// $username = "Ahmed";
+
+
+
+		// $x = $username;
+		// $x = 200;
+
+		// echo $username;
+		// echo "<h1>Heading</h1>";
+		// echo $x;
+		// echo "Hello World!";
+
+		$player1 = "ABC";
+		$player2 = "DEF";
+		$player3 = "GHI";
+		// echo ($player1);
+
+		// echo abs(-10) + print($player1);
+
+		// echo print("Asdasd");
+
+		$tictactoe = array(
+				array("x","0","x"),
+				array("0","x","0"),
+				array("x","x","0")
+
+		);
+		// echo 10 % 2;
+
+		// echo $tictactoe[2][2];
+		$player = array("ABC","DEF","GHI","JKL");
+		var_dump($player);
+
+		// echo "<pre>";
+		
+		// 	print_r($player);
+
+
+		// echo "</pre>";
+
+
+
+
+	?>
+
+</body>
+</html>
